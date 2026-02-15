33c33d319899ac22c5941d62784a2216c807035b
Josh Richardson <richardsonjosh03@gmail.com>
2026-02-15 12:12:03 -0800

    fixed safe area view error

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,37 +1,55 @@
 import React from "react";
 import HandSelector from "../components/HandSelector";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
     return (
-        <View style={styles.screen}>
+        <SafeAreaView style={styles.screen}>
             <View style={styles.gridArea}>
                 <HandSelector />
             </View>
-            <TouchableOpacity style={styles.bottomButtons}>
-                <Text style={styles.buttonText}>View Stats for selected day</Text>
-            </TouchableOpacity>
-        </View >
+            <View style={styles.bottomButtonContainer}>
+                <TouchableOpacity style={styles.bottomButtons}>
+                    <Text style={styles.buttonText}>Select Day</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bottomButtons}>
+                    <Text style={styles.buttonText}>View Stats for selected day</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.bottomButtons}>
+                    <Text style={styles.buttonText}>Submit Selected Hand</Text>
+                </TouchableOpacity>
+            </View>
+
+        </SafeAreaView >
     )
 }
 
 const styles = StyleSheet.create({
     screen: {
         flex: 1,
-        backgroundColor: "white",
+        backgroundColor: 'white',
     },
     gridArea: {
         flex: 1,
     },
+    bottomButtonContainer: {
+        flexDirection: 'row',
+        padding: 10,
+        gap: 8,
+    },
     bottomButtons: {
-        padding: 18,
-        backgroundColor: "#2e86ff",
-        alignItems: "center",
-        justifyContent: "center",
+        flex: 1,
+        height: 52,
+        backgroundColor: '#2e86ff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 10,
     },
     buttonText: {
-        color: "White",
-        fontWeight: "bold",
-        fontSize: 16,
+        color: 'White',
+        fontWeight: 'bold',
+        fontSize: 14,
+        textAlign: 'center',
     },
 })
